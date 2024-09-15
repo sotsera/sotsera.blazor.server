7ff04375b8e3e9d40e6ef33ce4b72f0d2c393f3a
Alessandro Ghidini <ghidello@users.noreply.github.com>
2024-09-15 18:11:06 +0200

    configure the test apis and reenable the integration test

--- samples/BlazorWebAppAutoGlobal/BlazorWebAppAutoGlobal/Program.cs
+++ samples/BlazorWebAppAutoGlobal/BlazorWebAppAutoGlobal/Program.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Alessandro Ghidini. All rights reserved.
 // SPDX-License-Identifier: MIT.
 
+using System.Net.Mime;
+using System.Text;
 using BlazorWebAppAutoGlobal.Client.Pages;
 using BlazorWebAppAutoGlobal.Components;
 
@@ -26,14 +28,37 @@ else
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
+app.MapGroup("api")
+    .AddTestApi();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorWebAppAutoGlobal.Client._Imports).Assembly);
 
 app.Run();
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class Program { }
+
+public static class TestApi
+{
+    public static RouteGroupBuilder AddTestApi(this RouteGroupBuilder builder)
+    {
+        builder.MapGet("/text", () => "Ciao!");
+
+        builder.MapGet("/json", () => TypedResults.Ok(new { Message = "Ciao!" }));
+
+        builder.MapGet("/html", () =>
+        {
+            const string content = "<html><body><h1>Ciao!</h1></body></html>";
+            return TypedResults.Content(content, MediaTypeNames.Text.Html, Encoding.UTF8);
+        });
+
+        return builder;
+    }
+}
--- test/Sotsera.Blazor.Server.Tests.Integration/BasicTests.cs
+++ test/Sotsera.Blazor.Server.Tests.Integration/BasicTests.cs
@@ -1,28 +1,28 @@
 // Copyright (c) Alessandro Ghidini. All rights reserved.
 // SPDX-License-Identifier: MIT.
 
-//using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-//namespace Sotsera.Blazor.Server.Tests.Integration;
+namespace Sotsera.Blazor.Server.Tests.Integration;
 
-//public class BasicTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
-//{
-//    [Theory]
-//    [InlineData("/", "text/html; charset=utf-8")]
-//    [InlineData("/counter", "text/html; charset=utf-8")]
-//    [InlineData("/weather", "text/html; charset=utf-8")]
-//    [InlineData("/appsettings.json", "application/json")]
-//    [InlineData("/_framework/blazor.web.js", "text/javascript")]
-//    [InlineData("/api/text", "text/plain; charset=utf-8")]
-//    [InlineData("/api/json", "application/json; charset=utf-8")]
-//    [InlineData("/api/html", "text/html; charset=utf-8")]
-//    public async Task GetEndpoints_ShouldReturnSuccessAndCorrectContentType(string url, string encoding)
-//    {
-//        var client = factory.CreateClient();
+public class BasicTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Theory]
+    [InlineData("/", "text/html; charset=utf-8")]
+    [InlineData("/counter", "text/html; charset=utf-8")]
+    [InlineData("/weather", "text/html; charset=utf-8")]
+    [InlineData("/appsettings.json", "application/json")]
+    [InlineData("/_framework/blazor.web.js", "text/javascript")]
+    [InlineData("/api/text", "text/plain; charset=utf-8")]
+    [InlineData("/api/json", "application/json; charset=utf-8")]
+    [InlineData("/api/html", "text/html; charset=utf-8")]
+    public async Task GetEndpoints_ShouldReturnSuccessAndCorrectContentType(string url, string encoding)
+    {
+        var client = factory.CreateClient();
 
-//        var response = await client.GetAsync(url);
+        var response = await client.GetAsync(url);
 
-//        response.EnsureSuccessStatusCode();
-//        Assert.Equal(encoding, response.Content.Headers.ContentType?.ToString());
-//    }
-//}
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(encoding, response.Content.Headers.ContentType?.ToString());
+    }
+}
